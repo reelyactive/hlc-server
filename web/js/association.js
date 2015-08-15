7e94fd4fa25e31a54c81516693b3eb83916e7815
Jeffrey Dungen <jeff@reelyactive.com>
2015-08-15 11:41:41 -0400

    Standardised with angular-style-guide

--- web/js/association.js
+++ web/js/association.js
@@ -1,17 +1,18 @@
-angular.module("association", [])
+angular.module("association", [ 'ui.bootstrap' ])
 
   // Association controller
   .controller("AssociationCtrl", function($scope, $http, $window) {
     $scope.success = false;
     $scope.device = {};
-    $scope.device.message = '';
+    $scope.alerts = [];
     $scope.device.href = '';
+    $scope.device.hasUrl = false;
 
     var url = $window.location.search.split("url=").pop().split('&').shift();
     var hasUrl = $window.location.search.indexOf("url=") != -1;
     if(hasUrl) {
       $scope.device.url = url;
-      // TODO: make url field readonly
+      $scope.device.hasUrl = true;
     }
 
     // ----- PUT /id/identifier -----
@@ -21,14 +22,21 @@ angular.module("association", [])
       $http.put('../associations/' + $scope.device.identifier + '/url', json)
         .success(function(data, status, headers, config) {
           $scope.success = true;
-          $scope.device.message = 'Successfully associated device';
+          var message = 'Successfully associated device identifier ' +
+                        $scope.device.identifier + ' with URL ' +
+                        $scope.device.url;
+          $scope.alerts.push( { type: "success", message: message } );
           $scope.device.href = 'contextnear/transmitter/' +
                                $scope.device.identifier;
-          $scope.device.query = 'Query /contextnear/transmitter/' +
-                                 $scope.device.identifier + ' now!';
+          $scope.device.query = 'Query context near the device';
         })
         .error(function(data, status, headers, config) {
-          $scope.device.message = "FAILED. Status code " + status;
+          var message = "Association failed. Status code " + status;
+          $scope.alerts.push( { type: "danger", message: message } );
         });
-    }
+    };
+
+    $scope.closeAlert = function(index) {
+      $scope.alerts.splice(index, 1);
+    };
   });
