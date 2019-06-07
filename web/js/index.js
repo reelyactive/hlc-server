3631c104749b8880db2ca845adb9950d95f0e8d8
jeffyactive <jeff@reelyactive.com>
2019-06-06 21:54:55 -0400

    Added basic memory and cpu load display

--- web/js/index.js
+++ web/js/index.js
@@ -6,10 +6,14 @@
 
 // Constants
 const STATISTICS_INTERVAL_MILLISECONDS = 1000;
+const STATUS_INTERVAL_MILLISECONDS = 2000;
+const STATUS_ROUTE = '/status';
 
 // DOM elements
 let raddecRate = document.querySelector('#raddecRate');
 let numTransmitters = document.querySelector('#numTransmitters');
+let mem = document.querySelector('#mem');
+let cpu = document.querySelector('#cpu');
 
 // Other variables
 let raddecCount = 0;
@@ -33,8 +37,37 @@ function updateStatistics() {
   raddecCount = 0;
 }
 
+// Update status
+function updateStatus() {
+  let statusUrl = baseUrl + STATUS_ROUTE;
+
+  getUrl(statusUrl, function(status, response) {
+    mem.textContent = Math.round(100 * response.status.memUseRatio);
+    cpu.textContent = Math.round(100 * response.status.cpuLoadRatio);
+  });
+}
+
+// GET the given URL
+function getUrl(url, callback) {
+  let httpRequest = new XMLHttpRequest();
+
+  httpRequest.onreadystatechange = function() {
+    if(httpRequest.readyState === XMLHttpRequest.DONE) {
+      return callback(httpRequest.status,
+                      JSON.parse(httpRequest.responseText));
+    }
+  };
+  httpRequest.open('GET', url);
+  httpRequest.setRequestHeader('Accept', 'application/json');
+  httpRequest.send();
+}
+
+// Startup functions
+updateStatus();
+
 // Periodic functions
 setInterval(updateStatistics, STATISTICS_INTERVAL_MILLISECONDS);
+setInterval(updateStatus, STATUS_INTERVAL_MILLISECONDS);
 
 
 
