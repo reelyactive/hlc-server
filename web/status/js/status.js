1453b6016b84b9a8db6ce632311d2d79a478dcc6
jeffyactive <jeff@reelyactive.com>
2019-06-06 21:28:11 -0400

    Added status route

---
+++ web/status/js/status.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright reelyActive 2019
+ * We believe in an open Internet of Things
+ */
+
+
+// DOM elements
+let mem = document.querySelector('#mem');
+let cpu = document.querySelector('#cpu');
+let jsonResponse = document.querySelector('#jsonResponse');
+
+
+// Initialisation: GET the status and display in DOM
+getStatus(window.location.href, function(status, response) {
+  jsonResponse.textContent = JSON.stringify(response, null, 2);
+  // TODO: handle the case of Not Found and Bad Request
+  mem.textContent = response.status.memUseRatio;
+  cpu.textContent = response.status.cpuLoadRatio;
+});
+
+
+// GET the associations
+function getStatus(url, callback) {
+  let httpRequest = new XMLHttpRequest();
+
+  httpRequest.onreadystatechange = function() {
+    if(httpRequest.readyState === XMLHttpRequest.DONE) {
+      return callback(httpRequest.status,
+                      JSON.parse(httpRequest.responseText));
+    }
+  };
+  httpRequest.open('GET', url);
+  httpRequest.setRequestHeader('Accept', 'application/json');
+  httpRequest.send();
+}
